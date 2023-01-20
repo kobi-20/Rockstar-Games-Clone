1b14f32c14473082fc3b387d957a812069fdecf9
kobi-20 <saifishadan.slm@gmail.com>
2023-01-20 21:46:25 +0530

    Header improve

--- JS/index.js
+++ JS/index.js
@@ -1,12 +1,30 @@
+//On Scroll header response logic
+
 const header = document.getElementById('main-header');
 let scroll_length = 0;
+let isNavOpen = false;
 window.addEventListener('scroll', () => {
-    if (document.documentElement.scrollTop > scroll_length) {
-        scroll_length = document.documentElement.scrollTop;
-        header.style.transform = 'translateY(-100%)'
+    if (isNavOpen == false) {
+        if (document.documentElement.scrollTop > scroll_length) {
+            scroll_length = document.documentElement.scrollTop;
+            header.style.transform = 'translateY(-100%)'
+        }
+        else {
+            scroll_length = document.documentElement.scrollTop;
+            header.style.transform = 'translateY(0)'
+        }
     }
-    else {
-        scroll_length = document.documentElement.scrollTop;
-        header.style.transform = 'translateY(0)'
-    }
+})
+
+// Social Club Login Nav Opener
+
+const socialBtn = document.getElementById('Social_Btn');
+const socialNav = document.getElementById('social-menu');
+socialBtn.addEventListener('click', () => {
+    socialNav.classList.toggle('club-nav-show')
+})
+
+const mainNavBtn = document.getElementById('main-navBtn');
+mainNavBtn.addEventListener('click', () => {
+    isNavOpen = (isNavOpen == false)? true  : false
 })
